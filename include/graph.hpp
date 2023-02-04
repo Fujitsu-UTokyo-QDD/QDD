58e7eb7eb71822e5e1ea03b6d90f9597a420e5ce
Li Shaowen <li-shaowen879@g.ecc.u-tokyo.ac.jp>
2023-02-04 09:41:57 +0900

    fix complex

--- include/graph.hpp
+++ include/graph.hpp
@@ -18,6 +18,18 @@
 
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 
+// Compute a power of two less than or equal to `n`
+static unsigned findPreviousPowerOf2(unsigned n)
+{
+    // do till only one bit is left
+    while (n & n - 1) {
+        n = n & n - 1;        // unset rightmost bit
+    }
+ 
+    // `n` is now a power of two (less than or equal to `n`)
+    return n;
+}
+
 class Node;
 class Executor;
 
