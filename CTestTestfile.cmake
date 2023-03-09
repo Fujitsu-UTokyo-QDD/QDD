79a2d801d829da4f1b41a5f230868cdd0a93abab
Li Shaowen <li-shaowen879@g.ecc.u-tokyo.ac.jp>
2023-03-09 10:33:43 +0900

    clear

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/swli0426/Code/QDD
-# Build directory: /home/swli0426/Code/QDD
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("src")
-subdirs("test")
