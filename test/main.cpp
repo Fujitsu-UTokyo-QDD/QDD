14638fb867af926e8db6f156d66f44cc688d780e
Li Shaowen <li-shaowen879@g.ecc.u-tokyo.ac.jp>
2023-03-15 09:48:58 +0900

    controls

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,8 @@
 #include "dd.h"
 #include <iostream>
 int main(int argc, char *argv[]) {
-    mEdge e1 = makeHybridGate(4, Hmat, 2, 1);
-    mEdge e2 = makeGate(4, Hmat, 2);
+    mEdge e1 = makeHybridGate(4, Hmat, 2, 1, Controls{Control{0}, Control{3}});
+    mEdge e2 = makeGate(4, Hmat, 2, Controls{Control{0}, Control{3}});
     e1.printMatrix();
     e2.printMatrix();
 }
