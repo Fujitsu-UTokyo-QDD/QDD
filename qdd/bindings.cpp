a018d98b199db69700727f1bcc3134139cebf563
Yusuke Kimura <yusuke-kimura@fujitsu.com>
2023-03-29 12:03:06 +0900

    Erase unrelated tests

--- qdd/bindings.cpp
+++ qdd/bindings.cpp
@@ -39,8 +39,8 @@ char _measureOneCollapsing(vEdge &rootEdge, const Qubit index, const bool assume
 }
 
 PYBIND11_MODULE(pyQDD, m){
-    py::class_<vEdge>(m, "vEdge");
-    py::class_<mEdge>(m, "mEdge");
+    py::class_<vEdge>(m, "vEdge").def("printVector",&vEdge::printVector).def("printVector_sparse",&vEdge::printVector_sparse);
+    py::class_<mEdge>(m, "mEdge").def("printMatrix",&mEdge::printMatrix);
     m.def("makeZeroState", makeZeroState);
     m.def("mv_multiply", mv_multiply).def("mm_multiply", mm_multiply);
 
