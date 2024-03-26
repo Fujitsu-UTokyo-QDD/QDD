56f202b8a580db7671e2cfbc0bf30577aa0014db
e60e256 <e60nayuta@g.ecc.u-tokyo.ac.jp>
2024-03-26 21:59:32 +0900

    A python script to obtain requirements for test from pyproject.toml (and install them all)

--- get_test_reqs.py
+++ get_test_reqs.py
@@ -1,3 +1,5 @@
+# A python script to obtain requirements for test from pyproject.toml (and install them all)
+# python3 -m pip install toml; python3 -m pip install $(python3 get_test_reqs.py)
 import toml
 
 data = toml.load(open("pyproject.toml"))
