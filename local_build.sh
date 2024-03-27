d21aa774abeaff7984fa0651a764b3b7672ccfa3
e60e256 <e60nayuta@g.ecc.u-tokyo.ac.jp>
2024-03-27 12:26:33 +0900

    feature: added local_build.sh

--- local_build.sh
+++ local_build.sh
@@ -1,7 +1,8 @@
-#cmake . -DCMAKE_BUILD_TYPE=Release
-#cmake --build . -j
-#python3 -m pip install build
-#python3 -m build
-python3 -m pip install toml; python3 -m pip install $(python3 get_test_reqs.py)
-python3 -m pytest test
-./test/qdd_test
+# .so build
+cmake . -DCMAKE_BUILD_TYPE=Release && cmake --build . -j
+
+# build (sdist and wheel)
+python3 -m pip install build && python3 -m build
+
+# Pytest
+python3 -m pip install toml && python3 -m pip install $(python3 get_test_reqs.py) && python3 -m pytest test && ./test/qdd_test
