66f175b3e9a9a94654acc5cb550b80526ff58222
Yusuke Kimura <kkyusuke@gmail.com>
2026-04-23 13:26:02 +0900

    Refactor GateTest assertions to use isNearlyEqual

--- test/test.cpp
+++ test/test.cpp
@@ -97,7 +97,7 @@ TEST(QddTest, GateTest) {
         mEdge m = makeGate(1, Tmat, 0);
         size_t dim;
         std_complex** mat = m.getMatrix(&dim);
-        ASSERT_TRUE(mat[0][0] == std::complex<double>(1.0, 0.0));
+        ASSERT_TRUE(isNearlyEqual(mat[0][0], {1.0, 0.0}));
         ASSERT_TRUE(mat[0][1] == std::complex<double>(0.0, 0.0));
         ASSERT_TRUE(mat[1][0] == std::complex<double>(0.0, 0.0));
         ASSERT_TRUE(isNearlyEqual(mat[1][1],
@@ -107,7 +107,7 @@ TEST(QddTest, GateTest) {
         mEdge m = makeGate(1, Tdagmat, 0);
         size_t dim;
         std_complex** mat = m.getMatrix(&dim);
-        ASSERT_TRUE(mat[0][0] == std::complex<double>(1.0, 0.0));
+        ASSERT_TRUE(isNearlyEqual(mat[0][0], {1.0, 0.0}));
         ASSERT_TRUE(mat[0][1] == std::complex<double>(0.0, 0.0));
         ASSERT_TRUE(mat[1][0] == std::complex<double>(0.0, 0.0));
         ASSERT_TRUE(isNearlyEqual(mat[1][1],
