72e69ddedface4beb306426bb0f8a4ad2fc1cc93
Yusuke Kimura <kkyusuke@gmail.com>
2026-04-23 11:30:25 +0900

    Use scikit_build_core

--- qdd/__init__.py
+++ qdd/__init__.py
@@ -1,4 +1,15 @@
-__version__ = "0.1.0"
+try:
+    from ._version import version as __version__
+except ImportError:
+    try:
+        from importlib.metadata import PackageNotFoundError, version
+    except ImportError:
+        __version__ = "0.0.0+unknown"
+    else:
+        try:
+            __version__ = version("qdd")
+        except PackageNotFoundError:
+            __version__ = "0.0.0+unknown"
 
 from .qdd_backend import QddBackend
 from .qdd_job import QddJob
